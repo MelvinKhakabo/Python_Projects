9ec19101ddb4251caaae48ab309925f513f2b3a7
MelvinKhakabo <melvinkhakabo@gmail.com>
2025-01-30 23:40:45 +0300

    50 Days of Python

--- 50_Days_of_Python/Day 3/register.py
+++ 50_Days_of_Python/Day 3/register.py
@@ -1,7 +1,11 @@
 #Day 3: Register Check
-#Write a function called register_check that checks how many students are in school. The function takes a dictionary as a parameter. If the 
-#student is in school, the dictionary says ‘yes’. If the student is not in 
+#Write a function called register_check that checks how many students are in school. The function takes a dictionary 
+# as a parameter. If the student is in school, the dictionary says ‘yes’. If the student is not in 
 #school, the dictionary says ‘no’. Your function should return the number 
 #of students in school. Use the dictionary below. Your function should 
 #return 3.
 
+register = {'Michael': 'yes', 'John': 'no', 'Peter': 'yes', 'Mary': 'yes'}
+def register_check(register):
+    return list(register.values()).count('yes')
+print(register_check(register))
