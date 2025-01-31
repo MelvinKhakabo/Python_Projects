883f31eef568d8378e32b3cf955c6e3d194b56d6
MelvinKhakabo <melvinkhakabo@gmail.com>
2025-01-31 19:40:24 +0300

    50 Days of Python

---
+++ 50_Days_of_Python/Day 11/equal.py
@@ -0,0 +1,8 @@
+#Write a function called equal_strings. The function takes two strings
+#as arguments and compares them. If the strings are equal (if they 
+#have the same characters and have equal length), it should return 
+#True, if they are not, it should return False. For example, ‘love’ and ‘evol’ should return True.
+
+def equal_strings(str1, str2):
+    return str1 == str2
+print(equal_strings('love', 'evol')) #True
