b3cd791344485af986a6f8705f3563e2d45f74a9
MelvinKhakabo <melvinkhakabo@gmail.com>
2025-02-03 21:59:53 +0300

    50 Days of Python

---
+++ 50_Days_of_Python/Day 17/generator.py
@@ -0,0 +1,12 @@
+#Write a function called user_name, that creates a username for the user. The function should ask a user to input their name. The 
+#function should then reverse the name and attach a randomly issued number between 0 – 9 at the end of the name. The function should 
+#return the username.
+
+import random
+
+def user_name():
+    name = input('Enter your name: ')
+    name = name[::-1]
+    return name + str(random.randint(0, 9))
+
+print(user_name())
