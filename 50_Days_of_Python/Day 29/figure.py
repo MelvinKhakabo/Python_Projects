c960894a8c66b54e0029910dc778090906dc0e0e
MelvinKhakabo <melvinkhakabo@gmail.com>
2025-02-04 23:14:30 +0300

    50 Days of Python

---
+++ 50_Days_of_Python/Day 29/figure.py
@@ -0,0 +1,13 @@
+#Write a function called middle_figure that takes two parameters a, 
+#and b. The two parameters are strings. The function joins the two 
+#strings and finds the middle element. If the combined string has a 
+#middle element, the function should return the element, otherwise,
+#return ‘no middle figure’. Use ‘ make love’ as an argument for a and
+#‘not wars’ as an argument for b. Your function should return ‘e’ as 
+#the middle element. Whitespaces should be removed.
+
+def middle_figure(a, b):
+    return a + b
+print(middle_figure('make love', 'not wars')) #makelovenotwars
+#The middle element is ‘e’
+#Output: e
