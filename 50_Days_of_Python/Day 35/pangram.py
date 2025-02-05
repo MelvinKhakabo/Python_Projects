db442f653424cd9ab558b1f0d67569dc6df0d61d
MelvinKhakabo <melvinkhakabo@gmail.com>
2025-02-05 19:31:18 +0300

    50 Days of Python

--- 50_Days_of_Python/Day 35/pangram.py
+++ 50_Days_of_Python/Day 35/pangram.py
@@ -1,4 +1,8 @@
-
+#Write a function called check_pangram that takes a string and checks if it is a pangram. 
+#A pangram is a sentence that contains all the letters of the alphabet. If it is a pangram, 
+#the function should return True, otherwise, return False. 
+# The following sentence is a pangram so it should return True:
+#'the quick brown fox jumps over a lazy dog
 
 sentence = "the quick brown fox jumps over the lazy dog"
 def is_pangram(sentence):
