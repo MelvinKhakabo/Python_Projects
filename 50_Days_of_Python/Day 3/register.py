c8abbb7eb5d1a20cff71b43541d922fd2870729c
MelvinKhakabo <melvinkhakabo@gmail.com>
2025-02-11 21:05:53 +0300

    50 Days of Python

--- 50_Days_of_Python/Day 3/register.py
+++ 50_Days_of_Python/Day 3/register.py
@@ -8,5 +8,6 @@
 register = {'Michael': 'yes', 'John': 'no', 'Peter': 'yes', 'Mary': 'yes'}
 def register_check(register):
     return list(register.values()).count('yes')
+print(register_check(register)) #3
 
 
