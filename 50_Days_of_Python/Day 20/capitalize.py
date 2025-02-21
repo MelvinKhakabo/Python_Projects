da43c4f1f04952b41e138eb9e1590957c07d3e75
MelvinKhakabo <melvinkhakabo@gmail.com>
2025-02-21 12:51:14 +0300

    50 Days of Python

--- 50_Days_of_Python/Day 20/capitalize.py
+++ 50_Days_of_Python/Day 20/capitalize.py
@@ -1,5 +1,4 @@
 #Write a function called capitalize. This function takes a string as an argument and capitalizes the first letter of each word. For example, 
 #‘i like learning’ becomes ‘I Like Learning’.
 
-def capitalize(string):
-    return string.title()
+
