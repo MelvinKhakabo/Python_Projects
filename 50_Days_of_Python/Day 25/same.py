93f15232e2bf3ac08e0a9aedd34782d7110ab6f4
MelvinKhakabo <melvinkhakabo@gmail.com>
2025-02-22 21:28:57 +0300

    50 Days of Python

--- 50_Days_of_Python/Day 25/same.py
+++ 50_Days_of_Python/Day 25/same.py
@@ -2,8 +2,10 @@
 #string, a list, or a tuple and checks if all the elements are the same. 
 #If the elements are the same, the function should return True. If not,
 #it should return False. For example, [‘Mary’, ‘Mary’, ‘Mary’] should 
-#return True.
+#return True and [‘Mary’, ‘Mary’, ‘John’] should return False.
 
 def all_the_same(data):
+    return(len(set(data)) == 1)
+print(all_the_same(['Mars', 'Mars', 'Mars']))
    
 
