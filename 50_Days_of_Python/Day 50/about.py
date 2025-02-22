c56cb8d25473506ab5f40ad0cbec927fdf246e3f
MelvinKhakabo <melvinkhakabo@gmail.com>
2025-02-22 21:43:00 +0300

    50 Days of Python

---
+++ 50_Days_of_Python/Day 50/about.py
@@ -0,0 +1,8 @@
+#In this challenge, you will get to create an app using Flask. Flask is a Python web framework. 
+#Learning to build websites with Flask is a very important skill to have if you want to get into web development. 
+#You will build an app using Python, HTML, and CSS. 
+#I recommend using Visual studio code for this challenge. 
+#You can still use whatever code editor you are comfortable with. 
+#You are going to create an app with two pages, the home page and the about page. 
+#Your website will have a navigation bar, so you can move between the home page and the about page. 
+#It will have a background image
