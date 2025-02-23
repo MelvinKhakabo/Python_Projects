7182333ce84a666cee2e89990975d2aa34cd3106
MelvinKhakabo <melvinkhakabo@gmail.com>
2025-02-23 16:38:19 +0300

    50 Days of Python

--- 50_Days_of_Python/Day 2/convert_add.py
+++ 50_Days_of_Python/Day 2/convert_add.py
@@ -2,6 +2,6 @@
 #Write a function called convert_add that takes a list of strings as an argument and converts it to integers and sums the list. For example 
 # [‘1’, ‘3’, ‘5’] should be converted to [1, 3, 5] and summed to 9.
  
-def convert_add(list):
+
 
 
