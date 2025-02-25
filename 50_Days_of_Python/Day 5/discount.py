17c6106ff48ed555d7985733eacb94f83bea4b0b
MelvinKhakabo <melvinkhakabo@gmail.com>
2025-02-25 22:05:20 +0300

    50 Days of Python

--- 50_Days_of_Python/Day 5/discount.py
+++ 50_Days_of_Python/Day 5/discount.py
@@ -3,5 +3,11 @@
 #should return the price after the discount. For example, if the user enters 150 as price and 15% as the discount, your function should 
 #return 127.5.
 
+def my_discount():
+    price = float(input("Enter the price of the product:")),
+    discount = float(input("Enter the percentage discount on the item:")),
+    price_after_discount = price - (price * discount/100),
+    return price_after_discount
+print(my_discount())  
 
    
