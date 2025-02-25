fc18694ca710d28f1e0575f96111e4dc817196f9
MelvinKhakabo <melvinkhakabo@gmail.com>
2025-02-25 22:16:14 +0300

    50 Days of Python

--- 50_Days_of_Python/Day 6/generator.py
+++ 50_Days_of_Python/Day 6/generator.py
@@ -2,7 +2,9 @@
 #the code should return everything before the @ sign as their user name. For example, if someone enters ben@gmail.com, the code 
 #should return ben as their user name.
 
-
-    
+def user_name(email):
+    email = str(input("Enter your rmail:")),
+    return email.split("@")[0]
+print(user_name("ben@gmail.com"))  # ben
 
 
