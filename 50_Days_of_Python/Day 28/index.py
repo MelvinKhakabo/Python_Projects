93c1dce29ab81b80cef5e4f572c1afc20f3c5be0
MelvinKhakabo <melvinkhakabo@gmail.com>
2025-02-26 22:53:39 +0300

    50 Days of Python

--- 50_Days_of_Python/Day 28/index.py
+++ 50_Days_of_Python/Day 28/index.py
@@ -2,6 +2,9 @@
 #as a parameter and returns the positions or indexes of all lower 
 #letters in the string. For example ‘LovE’ should return [1,2].
 
+def index_position(a):
+    return [i for i in range(len(a)) if a[i].islower()]
+
 
   
 
