c81b3a0c3aebe57504bba5273bab27d8c294f4d6
MelvinKhakabo <melvinkhakabo@gmail.com>
2025-02-28 06:45:44 +0300

    50 Days of Python

--- 50_Days_of_Python/Day 51/loops2.py
+++ 50_Days_of_Python/Day 51/loops2.py
@@ -8,3 +8,11 @@ areas = [11.25, 18.0, 20.0, 10.75, 9.50]
 for area in areas:
     print(area)
 
+#Using indexing to access elements in a for loop:
+# areas list
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Change for loop to use enumerate() and update print()
+for index, a in enumerate(areas):
+    print("index" + str(index) + ":" + str(a))
+
