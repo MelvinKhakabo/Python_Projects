56ea1b2f40077f0c5081165b1f73a988cc0b6f8e
MelvinKhakabo <melvinkhakabo@gmail.com>
2025-02-28 07:20:34 +0300

    50 Days of Python

---
+++ 50_Days_of_Python/Day 51/loops3.py
@@ -0,0 +1,5 @@
+#iterating though dictionaries and numpy arrays
+#dictionaries req methods, and numpy arrays req functions(nditer). In Python3 you need items method to iterate through a dictionary.
+
+#Iterating over a dictionary(unordered)
+#Write a for loop that goes through each key:value pair of europe. On each iteration, "the capital of x is y" should be printed out, where x is the key and y is the value of the pair.
