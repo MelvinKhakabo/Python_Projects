5488da223a43cd8bfc96eb0bb85ad90413b62b48
MelvinKhakabo <melvinkhakabo@gmail.com>
2025-02-28 07:49:41 +0300

    50 Days of Python

--- 50_Days_of_Python/Day 51/loops3.py
+++ 50_Days_of_Python/Day 51/loops3.py
@@ -10,3 +10,22 @@ europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin',
 # Iterate over europe
 for x, y in europe.items():
     print("the capital of " + x  + " is " + str(y))
+
+
+
+#Loop over Numpy array
+#1d numpy array is similar to a list(for x in my_array :  ...) 
+#A 2d numpy array is similar to a list of lists.
+#for x in np.nditer(my_array) : ...
+
+# Import numpy as np
+import numpy as np
+
+# For loop over np_height( 1D array)
+#inches = "inches"
+#for height in np_height:  
+#    print(f"{height} {inches}")  
+
+# For loop over np_baseball (2D array)
+#for x in np.nditer(np_baseball):
+#    print(x)
