8b5061ce93afc8b09318fd9a44a8515409a142d6
MelvinKhakabo <melvinkhakabo@gmail.com>
2025-02-28 06:57:41 +0300

    50 Days of Python

--- 50_Days_of_Python/Day 51/loops2.py
+++ 50_Days_of_Python/Day 51/loops2.py
@@ -27,4 +27,15 @@ areas = [11.25, 18.0, 20.0, 10.75, 9.50]
 # Code the for loop
 for index, area in enumerate(areas):
     print("room " + str(index + 1) + ": " + str(area))
-    
+
+
+
+#Loop over list of lists
+#Write a for loop that goes through each sublist of house and prints out the x is y sqm, where x is the name of the room and y is the area of the room.
+
+# house list of lists
+house = [["hallway", 11.25], 
+         ["kitchen", 18.0], 
+         ["living room", 20.0], 
+         ["bedroom", 10.75], 
+         ["bathroom", 9.50]]
