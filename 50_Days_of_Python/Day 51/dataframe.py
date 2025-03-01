844dadaaece1b69bbd37b73a098e4fbf81328349
MelvinKhakabo <melvinkhakabo@gmail.com>
2025-03-01 12:34:45 +0300

    50 Days of Python

--- 50_Days_of_Python/Day 51/dataframe.py
+++ 50_Days_of_Python/Day 51/dataframe.py
@@ -18,3 +18,16 @@
 # Adapt for loop
 #for lab, row in cars.iterrows() :
 #    print(lab + ": " + str(row['cars_per_cap']))
+
+
+#add column
+# Import cars data
+#import pandas as pd
+#cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Code for loop that adds COUNTRY column
+#for lab, row in cars.iterrows():
+#    cars.loc[lab, "name_length"] = len(row["country"])
+
+# Print cars
+#print(cars)
