ddf23c4b0948c355f45e6bddf71fcb27ffae35e5
MelvinKhakabo <melvinkhakabo@gmail.com>
2025-03-01 12:27:25 +0300

    50 Days of Python

---
+++ 50_Days_of_Python/Day 51/dataframe.py
@@ -0,0 +1,20 @@
+#looping over a Dataframe
+#import pandas as pd
+# Import cars data
+#import pandas as pd
+#cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Iterate over rows of cars
+#for lab, row in cars.iterrows():
+#    print({lab})
+#    print(row)
+
+#use iterators lab and row, to adopt the code to print out the row label and the cars per capita
+# Iterate over rows of cars
+# Import cars data
+#import pandas as pd
+#cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Adapt for loop
+#for lab, row in cars.iterrows() :
+#    print(lab + ": " + str(row['cars_per_cap']))
