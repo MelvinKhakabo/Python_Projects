d0d16c8997e8819dc90c156223be0cc2d043c023
MelvinKhakabo <melvinkhakabo@gmail.com>
2025-03-04 23:02:51 +0300

    50 Days of Python

--- 50_Days_of_Python/Day 51/age.py
+++ 50_Days_of_Python/Day 51/age.py
@@ -1,8 +1,1 @@
-def printCategory(age):  
-    if age > 18:  
-        print('Adult')  
-    elif age > 65:  
-        print('Senior Citizen')  
-    else:  
-        print('child')  
-printCategory(20)
+#age 
