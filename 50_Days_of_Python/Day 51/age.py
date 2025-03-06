4974458965beb9289e5d4cc7a59e501642c73e15
MelvinKhakabo <melvinkhakabo@gmail.com>
2025-03-06 23:13:10 +0300

    50 Days of Python

--- 50_Days_of_Python/Day 51/age.py
+++ 50_Days_of_Python/Day 51/age.py
@@ -1,9 +1,4 @@
 #age 
-def age_in_minutes(age):
-    return age * 525600 # 525600 minutes in a year  (60 * 24 * 365)
-print(age_in_minutes(25)) # 13140000
-print(age_in_minutes(40)) # 21024000
-print(age_in_minutes(50)) # 26280000
 
 
 
