b362947927ff04a70e5490c2e053c65e211078d2
MelvinKhakabo <melvinkhakabo@gmail.com>
2025-03-07 23:04:05 +0300

    50 Days of Python

--- 50_Days_of_Python/Day 51/age.py
+++ 50_Days_of_Python/Day 51/age.py
@@ -4,5 +4,5 @@ def age_program():
     print("Welcome to the age program!")
     user_age = input("Enter your age: ")
     age_seconds = int(user_age) * 365 * 24 * 60 * 60
-    print(f"Your age in seconds is {age_seconds}.")
+print(f"Your age in seconds is {age_seconds}.")
 
