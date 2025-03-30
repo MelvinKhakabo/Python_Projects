b651ba41a8916d7300a77e01733e6a1a40253b91
MelvinKhakabo <melvinkhakabo@gmail.com>
2025-03-30 17:34:58 +0300

    Python projects

--- 50_Days_of_Python/Day 4/float.py
+++ 50_Days_of_Python/Day 4/float.py
@@ -3,5 +3,12 @@
 #If you pass (12.1, 23) as an argument, your function should return a 1.
 
 
-
+def only_floats(a, b):
+    if isinstance(a, float) and isinstance(b, float):
+        return 2
+    elif isinstance(a, float) or isinstance(b, float):
+        return 1
+    else:
+        return 0
+print(only_floats(12.1, 23))  # 1
 
