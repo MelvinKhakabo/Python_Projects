32d7ae46f5957cd1933a967b7af24fcd2a135d4f
MelvinKhakabo <melvinkhakabo@gmail.com>
2025-04-04 21:55:26 +0300

    python projects

--- 50_Days_of_Python/Day 1/div_sq.py
+++ 50_Days_of_Python/Day 1/div_sq.py
@@ -3,4 +3,14 @@
 #and returns the square root of the number if it is divisible by 5, returns its remainder if it is not divisible by 5. 
 #For example, if you pass 10 as an argument, then your function should return 3.16 as the square root.
 
+def divide_or_square(num):
+    """
+    This function takes a number as input and returns the square root of the number if it is divisible by 5.
+    Otherwise, it returns the remainder when the number is divided by 5.
+    """
+    if num % 5 == 0:
+        return round(num ** 0.5, 2)  # Return square root rounded to 2 decimal places
+    else:
+        return num % 5  # Return remainder when divided by 5
 
+print(divide_or_square(10))  # Output: 3.16 (square root of 10)
