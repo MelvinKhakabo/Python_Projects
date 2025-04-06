ea5275b46a05fd553240b74787e2b1f0f025a38a
MelvinKhakabo <melvinkhakabo@gmail.com>
2025-04-06 15:10:07 +0300

    python projects

--- 50_Days_of_Python/Day 6/generator.py
+++ 50_Days_of_Python/Day 6/generator.py
@@ -2,9 +2,6 @@
 #the code should return everything before the @ sign as their user name. For example, if someone enters ben@gmail.com, the code 
 #should return ben as their user name.
 
-def user_name():
-    email = str(input("Enter your email: "))
-    return email.split("@")[0]
-print(user_name())
+
 
 
