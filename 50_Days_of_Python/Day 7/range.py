af701f6a9434603bf15cc8559debaa5526b3f770
MelvinKhakabo <melvinkhakabo@gmail.com>
2025-04-27 09:56:59 +0300

    python projects

--- 50_Days_of_Python/Day 7/range.py
+++ 50_Days_of_Python/Day 7/range.py
@@ -1,9 +1,7 @@
 #Write a function called string_range that takes a single number and returns a string of its range. The string characters should be 
 #separated by dots(.) For example, if you pass 6 as an argument, your function should return ‘0.1.2.3.4.5’.
 
-def string_range(num):    
-    return '.'.join(str(i) for i in range(num))
-print(string_range(6)) # Output: '0.1.2.3.4.5'
+
 
 
 
