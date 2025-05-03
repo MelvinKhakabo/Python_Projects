9cbf8e0b42bf285facea885aaa52764b8b47a805
MelvinKhakabo <melvinkhakabo@gmail.com>
2025-05-03 22:57:44 +0300

    python projects

--- 50_Days_of_Python/Day 6/generator.py
+++ 50_Days_of_Python/Day 6/generator.py
@@ -3,9 +3,5 @@
 #should return ben as their user name.
 
 
-
-
-def user_name():
-    email = str(input("Enter your email: "))
-    return email.split("@")[0]
-print(user_name())
+# The code should also handle the case where the user enters an invalid email address.
+# For example, if someone enters ben@gmail, the code should return an error message saying that the email address is invalid.
