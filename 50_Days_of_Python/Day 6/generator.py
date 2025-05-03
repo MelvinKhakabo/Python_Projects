d20987f391f3cbf04d927d92bd35856bf6bf244e
MelvinKhakabo <melvinkhakabo@gmail.com>
2025-05-03 23:00:21 +0300

    python projects

--- 50_Days_of_Python/Day 6/generator.py
+++ 50_Days_of_Python/Day 6/generator.py
@@ -5,4 +5,11 @@
 
 # The code should also handle the case where the user enters an invalid email address.
 # For example, if someone enters ben@gmail, the code should return an error message saying that the email address is invalid.
+def user_name(email):
+    if "@" not in email or "." not in email.split("@")[1]:
+        return "Invalid email address"
+    else:
+        return email.split("@")[0]
 
+email = input("Enter your email: ")
+print(user_name(email))
