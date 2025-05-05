246b4560a2acc9a00f8086caf28f0f7c4375e6a1
MelvinKhakabo <melvinkhakabo@gmail.com>
2025-05-05 09:56:29 +0300

    python projects

--- 50_Days_of_Python/Day 8/eve.py
+++ 50_Days_of_Python/Day 8/eve.py
@@ -2,3 +2,4 @@
 #returns the difference between the largest even number in the list and the smallest odd number in the list. For example, 
 #if you pass [1,2,4,6] as an argument the function should return 6 -1= 5.
 
+
