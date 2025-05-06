c33bf41418ffc54787bce04bb3a78a967e0afdf9
MelvinKhakabo <melvinkhakabo@gmail.com>
2025-05-06 22:10:09 +0300

    python projects

--- 50_Days_of_Python/Day 8/eve.py
+++ 50_Days_of_Python/Day 8/eve.py
@@ -2,7 +2,6 @@
 #returns the difference between the largest even number in the list and the smallest odd number in the list. For example, 
 #if you pass [1,2,4,6] as an argument the function should return 6 -1= 5.
 
-
 def odd_even(lst):
     evens = [i for i in lst if i % 2 == 0]
     odds = [i for i in lst if i % 2 != 0]
